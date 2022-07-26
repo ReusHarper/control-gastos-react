bdfb869df1f7623aaa20873400043bd60e42be7c
ReusHarper <artoby99@hotmail.com>
2022-07-25 23:31:46 -0500

    Creación de componente de NuevoPresupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [presupuesto, setPresupuesto] = useState(0);
+    const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+    
+    return (
+        <div>
+            <Header
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+                isValidPresupuesto={isValidPresupuesto}
+                setIsValidPresupuesto={setIsValidPresupuesto}
+            />
+        </div>
+    );
+};
 
-  return (
-    <h1>Control de gastos</h1>
-  )
-}
-
-export default App
+export default App;
