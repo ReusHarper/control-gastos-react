360cfddb4140bb659269e30a3e3b0f1d180c2dae
ReusHarper <reusharper@comunidad.unam.mx>
2022-08-09 17:23:51 -0500

    Creacion de modal que permite registrar los productos o servicios gastados

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import { useState } from 'react';
 import Header from './components/Header';
+import Modal from './components/Modal';
+import { generarId } from './helpers';
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
     const [presupuesto, setPresupuesto] = useState(0);
     const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
-    const [modal, setModal] = useState(false);
     
+    const [modal, setModal] = useState(false);
+    const [animarModal, setAnimarModal] = useState(false);
+    
+    const [gastos, setGastos] = useState([]);
+
     const handleNuevoGasto = () => {
         setModal(true);
+        
+        setTimeout(() => {
+            setAnimarModal(true);
+        }, 500);
     };
+
+    const guardarGasto = gasto => {
+        gasto.id = generarId();
+        setGastos([ ... gastos, gasto ]);
+
+        setAnimarModal(false);
+        
+        setTimeout(() => {
+            setAnimarModal(false);
+        }, 500);
+    }
     
     return (
         <div>
@@ -32,9 +53,14 @@ function App() {
             )}
 
             {/* Si se da click en el botón + entonces mostramos una ventana modal para agregar un nuevo gasto */}
-            {
-                modal && <p>Desde modal</p>
-            }
+            {modal && (
+                <Modal
+                    setModal={setModal}
+                    animarModal={animarModal}
+                    setAnimarModal={setAnimarModal}
+                    guardarGasto={guardarGasto}
+                />
+            )}
         </div>
     );
 };
